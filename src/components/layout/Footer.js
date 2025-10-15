8e8ebc4989c4ba9150b617861f1fbbd6ee6b7aec
dockie <docktype2024@gmail.com>
2025-10-15 20:12:45 +0100

    Finalizando meu-projeto

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,17 @@
+import {FaFacebook, FaInstagram, FaLinkedinIn} from 'react-icons/fa'
+import styles from './Footer.module.css'
 function Footer(){
-    return <footer>Rodape</footer>
+    return (
+    <footer>
+        <ul className={styles.social_list}>
+            <li>
+                <FaFacebook/>
+                <FaInstagram/>
+                <FaLinkedinIn/>
+            </li>
+        </ul>   
+        <p>Nosso rodape</p>     
+    </footer>
+)
 }
 export default Footer
